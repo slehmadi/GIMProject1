d7f1d120a2fe97a605adf55f5cb79c1457bc4741
Ishlah Muhammadi <ishlahims@gmail.com>
2021-10-05 17:23:43 +0700

    Adding Additional file

---
+++ README.txt
@@ -0,0 +1,5 @@
+Firstly, Thank You for Iqi who gave tutorial, and thank you to O_LOBSTER who make an awesome platform
+
+
+credits:
+O_LOBSTER: https://o-lobster.itch.io/
--- script/Player.gd
+++ script/Player.gd
@@ -1,35 +1,63 @@
 extends KinematicBody2D
 
-var velocity : Vector2
+export var velocity : Vector2
 var axis : Vector2
 export var moveBy = 500
+export var jumpForce = -300
+export var gravity = 320
+onready var camera = $Camera2D
+
 
 func _ready():
-	pass 
+	camera.limit_left = 0
+	camera.limit_bottom = 690
+	camera.limit_top = 50
 
-func _process(delta):
-	pass
+#func _process(delta):
+#	pass
 
 func _physics_process(delta):
-	velocity.x = 0
 	axis.x = 0
+	inputPlayer(delta)
+	playerAct(delta)
 	
 	
+
+func inputPlayer(delta):
 	if Input.is_action_pressed("RIGHT"):
 		axis.x += 1
 	if Input.is_action_pressed("LEFT"):
 		axis.x -= 1
 	if Input.is_action_pressed("JUMP") and is_on_floor():
-		velocity.y = -300
+		velocity.y = jumpForce
+	if Input.is_action_pressed("DOWN") and !is_on_floor():
+		velocity.y = gravity * 2
 	
+func playerAct(delta):
 	if (axis.x):
-		velocity.x = lerp(velocity.x, axis.x * moveBy, 0.3)
+		if is_on_floor() and !is_on_wall():
+			$AnimatedSprite.animation = "walk"
+		if is_on_floor() and is_on_wall():
+			$AnimatedSprite.animation = "push"
+		if !is_on_floor():
+			jumpCondition()
+		velocity.x = lerp(velocity.x, axis.x * moveBy, 0.1)
+		if (axis.x < 0):
+			$AnimatedSprite.flip_h = true
+		if (axis.x > 0):
+			$AnimatedSprite.flip_h = false
 	else:
+		if is_on_floor():
+			$AnimatedSprite.animation = "idle"
+		else:
+			jumpCondition()
 		velocity.x = lerp(velocity.x, 0, 0.1)
 	
-	velocity.y += 300 * delta
-	
+	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2(0, -1))
-	
 
-	
+func jumpCondition():
+	if (velocity.y < 0):
+		$AnimatedSprite.animation = "jumpUp"
+	else:
+		$AnimatedSprite.animation = "jumpDown"
